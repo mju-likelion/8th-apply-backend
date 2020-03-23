1a41f8ce3a919b5dd087552cb58dba188395172f
yuyaebean <yuyaebean@gmail.com>
2020-03-24 00:24:09 +0900

    [Volunteer] Edit volunteer resolver

--- src/graphql/Volunteer/volunteer/volunteer.resolvers.ts
+++ src/graphql/Volunteer/volunteer/volunteer.resolvers.ts
@@ -1,16 +1,16 @@
 import { prisma, Volunteer } from '../../../../generated/prisma-client';
 
 interface Args {
-  email: string;
+  sid: string;
   password: string;
 }
 
 export default {
   Query: {
-    volunteer: async (_: any, { email, password }: Args) => {
-      const volunteer: Volunteer = await prisma.volunteer({ email });
+    volunteer: async (_: any, { sid, password }: Args) => {
+      const volunteer: Volunteer = await prisma.volunteer({ sid });
 
-      if (volunteer === null) throw Error('Email not exist!');
+      if (volunteer === null) throw Error('Student ID not exist!');
       else if (volunteer.password !== password)
         throw Error('Password is not same!');
 
